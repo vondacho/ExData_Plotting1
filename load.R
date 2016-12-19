69b822a93b9416d833096a6494e9d63a74b0783b
vondacho <olivier.vondach@obya.ch>
2016-12-19 08:30:55 +0100

    added download of dataset file

--- load.R
+++ load.R
@@ -1,4 +1,13 @@
-# Loads the energy data for the explorative analysis
+# Downloads the energy dataset file
+
+energy_data_origin.url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+energy_data_origin.zip <- "household_power_consumption.zip"
+if (!file.exists(energy_data_origin.zip))
+    download.file(energy_data_origin.url, destfile = energy_data_origin.zip, method = "curl")
+
+unzip(energy_data_origin.zip)
+
+# Loads the energy dataset for the exploratory analysis
 
 energy_data_origin<-read.csv("household_power_consumption.txt",
                       sep=";", na.strings = "?",
